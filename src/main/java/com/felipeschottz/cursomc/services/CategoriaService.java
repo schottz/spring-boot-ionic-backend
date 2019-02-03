144e6e3faf4d145bb4ef8d8bed1e1a5913a047e1
Felipe Schottz <schottz@gmail.com>
2019-02-03 16:23:33 -0200

    Ajuste no endpoint/categorias/{id}

--- src/main/java/com/felipeschottz/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipeschottz/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.felipeschottz.cursomc.domain.Categoria;
 import com.felipeschottz.cursomc.repositories.CategoriaRepository;
+import com.felipeschottz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado " + id);
+		}
 		return obj;
 	}
 }
