55698546d41f895706abf70301757d5f38f34cc8
Felipe Schottz <schottz@gmail.com>
2019-02-04 14:36:57 -0200

    Atualizacao utilizando somente jsonignore

--- src/main/java/com/felipeschottz/cursomc/domain/Cliente.java
+++ src/main/java/com/felipeschottz/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.felipeschottz.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,13 +29,13 @@ public class Cliente  implements Serializable{
 	private String cpfCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	@ElementCollection
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
