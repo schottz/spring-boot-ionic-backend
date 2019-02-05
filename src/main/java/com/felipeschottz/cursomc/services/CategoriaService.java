60137b1eb66393d1d358fe52469a1bfc270de4f3
Felipe Schottz <schottz@gmail.com>
2019-02-05 15:03:44 -0200

    deletando categoria com DELETE

--- src/main/java/com/felipeschottz/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipeschottz/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.felipeschottz.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.felipeschottz.cursomc.domain.Categoria;
 import com.felipeschottz.cursomc.repositories.CategoriaRepository;
+import com.felipeschottz.cursomc.services.exceptions.DataIntegrityException;
 import com.felipeschottz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,7 +18,7 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado " + id);
+			throw new ObjectNotFoundException("Objeto não encontrado. ID: " + id);
 		}
 		return obj;
 	}
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
